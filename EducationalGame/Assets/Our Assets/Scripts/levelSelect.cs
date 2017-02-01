12ab5e4cbf908481b2136fefa2b66d39bfd183af
Andrew Steven Chau <aschau24@gmail.com>
2017-01-31 23:48:49 -0800

    added some minimal setup of menus as reference for team.

---
+++ EducationalGame/Assets/Our Assets/Scripts/levelSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void backButton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
---
+++ EducationalGame/Assets/Our Assets/Scripts/mainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playButton()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void settingsButton()
+    {
+
+    }
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+}
