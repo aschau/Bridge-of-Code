73e74949b56822f94e3603bf20c1240a7521fa39
thuctonhuyen <thuctonhuyen@gmail.com>
2017-02-18 07:01:08 -0800

    addHelpMenu

---
+++ EducationalGame/Assets/Our Assets/Scripts/helpMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class helpMenu : MonoBehaviour
+{
+    private GameObject level1;
+
+    void Awake()
+    {
+        this.level1 = GameObject.Find("Level 1");
+    }
+    // Use this for initialization
+    void Start () {
+        this.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void exitSettings()
+    {
+        this.level1.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
