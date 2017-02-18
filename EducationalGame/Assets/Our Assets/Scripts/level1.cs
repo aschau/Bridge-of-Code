cb42239e3113506542b8874a43efc9c71c877858
thuctonhuyen <thuctonhuyen@gmail.com>
2017-02-18 07:01:08 -0800

    addHelpMenu

---
+++ EducationalGame/Assets/Our Assets/Scripts/level1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level1 : MonoBehaviour
+{
+    private GameObject helpMenu;
+    void Awake()
+    {
+        this.helpMenu = GameObject.Find("Help Menu");
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void helpButton()
+    {
+        this.helpMenu.SetActive(true);
+        //this.gameObject.SetActive(false);
+    }
+
+    public void exitFromHelpButton()
+    {
+        this.helpMenu.SetActive(false);
+        SceneManager.LoadScene("Level 1");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
