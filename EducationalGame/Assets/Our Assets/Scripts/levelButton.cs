8134771677a06439f7eea45c2b00c6a859899870
ahirota <ahirota@uci.edu>
2017-02-20 20:17:01 -0800

    Solved Level Interactibility with Static Variable
    See Title

--- EducationalGame/Assets/Our Assets/Scripts/levelButton.cs
+++ EducationalGame/Assets/Our Assets/Scripts/levelButton.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class levelButton : MonoBehaviour {
 
+	public static int levelCount = 1;
+	public Button myButton;
+
 	// Use this for initialization
 	void Start () {
-		
+		myButton = this.GetComponent<Button>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (levelCount == 1)
+		{
+			myButton.interactable = true;
+		}
 	}
 
     public void loadLevel()
