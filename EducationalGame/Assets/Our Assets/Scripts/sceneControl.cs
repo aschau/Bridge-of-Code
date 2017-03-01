435d1b90cef73e3cca8cb79c965492d1d89cec76
Andrew Steven Chau <aschau24@gmail.com>
2017-02-28 19:18:46 -0800

    started setting up sceneControl.cs

---
+++ EducationalGame/Assets/Our Assets/Scripts/sceneControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneControl : MonoBehaviour {
+    private timer timerObject;
+    public bool paused;
+    void Awake()
+    {
+        if (GameObject.Find("Timer"))
+        {
+            timerObject = GameObject.Find("Timer").GetComponent<timer>();
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void togglePause()
+    {
+        this.paused = !this.paused;
+        if (this.paused)
+        {
+            if (this.timerObject)
+            {
+                timerObject.paused = true;
+            }
+        }
+        else
+        {
+            if (this.timerObject)
+            {
+                timerObject.paused = false;
+            }
+        }
+    }
+}
