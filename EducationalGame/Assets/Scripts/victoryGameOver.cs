644b414daf252d9ebb4edbcfa52c702d3b39b483
thuctonhuyen <thuctonhuyen@gmail.com>
2017-03-02 21:10:41 -0800

    add victory, gameover

---
+++ EducationalGame/Assets/Scripts/victoryGameOver.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class victoryGameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadVictory()
+    {
+
+
+
+    }
+
+
+    public void loadGameOver()
+    {
+        
+    }
+
+    public void load(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+		
+	}
+}
